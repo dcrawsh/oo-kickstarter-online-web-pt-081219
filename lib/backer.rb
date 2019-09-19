5507fe942eab671399ebdf2bf4aeb711b7e9af56
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-19 17:56:02 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,14 @@
 class Backer
 attr_reader :name  
+ 
 
 def initialize(name)
 @name = name 
-@backed_projects = Array.new  
+@backed_projects = Array.new 
 end 
+
+def backed_projects
+@backed_projects
+end 
+
 end 
